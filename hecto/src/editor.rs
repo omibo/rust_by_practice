5f41857cb7da267a0e72fb0ed6987b7fe7fcc133
omibo <omidbodaghi00@gmail.com>
2024-02-16 13:17:37 -0700

    Add editor module

---
+++ hecto/src/editor.rs
@@ -0,0 +1,32 @@
+use std::io::{self, stdout}; 
+use termion::input::TermRead; 
+use termion::event::Key;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key{
+                    Key::Char(c) => {            
+                        if c.is_control() {            
+                            println!("{:?}\r", c as u8);            
+                        } else {            
+                            println!("{:?} ({})\r", c as u8, c);            
+                        }
+                    }
+                    Key::Ctrl('q') => break,
+                    _ => println!("{:?}\r", key),
+                },
+                Err(err) => panic!("{}", err),
+            }
+        }
+    }
+
+    pub fn default() -> Self {
+        Editor {}
+    }
+}
--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,24 +1,8 @@
-use std::io::{self, stdout}; 
-use termion::input::TermRead; 
-use termion::event::Key;
-use termion::raw::IntoRawMode;
+mod editor;
 
-fn main() {            
-    let _stdout = stdout().into_raw_mode().unwrap();
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key{
-                Key::Char(c) => {            
-                    if c.is_control() {            
-                        println!("{:?}\r", c as u8);            
-                    } else {            
-                        println!("{:?} ({})\r", c as u8, c);            
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => panic!("{}", err),
-        }
-    }
+use editor::Editor;
+
+fn main() {
+    let editor = Editor::default();
+    editor.run();
 }
