04c1c05f9a8d95eea97cbdef0163180d695d2860
omibo <omidbodaghi00@gmail.com>
2024-02-16 13:07:59 -0700

    Read keys instead of bytes

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,27 +1,24 @@
-use std::io::{self, stdout, Read}; 
-use termion::raw::IntoRawMode;  
-         
-fn to_ctrl_byte(c: char) -> u8 {            
-    let byte = c as u8;            
-    byte & 0b0001_1111            
-}
+use std::io::{self, stdout}; 
+use termion::input::TermRead; 
+use termion::event::Key;
+use termion::raw::IntoRawMode;
 
 fn main() {            
     let _stdout = stdout().into_raw_mode().unwrap();
-    for b in io::stdin().bytes() {    
-        match b {
-            Ok(b) => {            
-                let c: char = b as char;            
-                if c.is_control() {            
-                    println!("{:?} \r", b);            
-                } else {            
-                    println!("{:?} ({})\r", b, c);            
-                }            
-                if b == to_ctrl_byte('q') {            
-                    break;            
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key{
+                Key::Char(c) => {            
+                    if c.is_control() {            
+                        println!("{:?}\r", c as u8);            
+                    } else {            
+                        println!("{:?} ({})\r", c as u8, c);            
+                    }
                 }
-            }
+                Key::Ctrl('q') => break,
+                _ => println!("{:?}\r", key),
+            },
             Err(err) => panic!("{}", err),
-        }           
+        }
     }
 }
