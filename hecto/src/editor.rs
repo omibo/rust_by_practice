7fa5c5d1620bbc995b81ee79a43f80e9ffa98465
omibo <omidbodaghi00@gmail.com>
2024-02-17 16:17:17 -0700

    Add reading file functionality

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,5 +1,8 @@
 use crate::Terminal;
+use crate::Document;
+use crate::Row;
 use termion::event::Key;
+use std::env;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -7,8 +10,10 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    document: Document,
 }
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize, 
     pub y: usize,
@@ -36,10 +41,18 @@ impl Editor {
     }
 
     pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {            
+            let file_name = &args[1];            
+            Document::open(&file_name).unwrap_or_default()            
+        } else {            
+            Document::default()            
+        };
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: Position { x: 0, y: 0 },
+            cursor_position: Position::default(),
+            document,
         }
     }
 
@@ -54,11 +67,21 @@ impl Editor {
         println!("{}\r", welcome_message);            
     }
 
+    pub fn draw_row(&self, row: &Row) {            
+        let start = 0;            
+        let end = self.terminal.size().width as usize;            
+        let row = row.render(start, end);            
+        println!("{}\r", row)            
+    }
+
     fn draw_rows(&self) {
-        let height = self.terminal.size().height;
-        for row in 0..self.terminal.size().height-1 {
+        let height: u16 = self.terminal.size().height;
+        for terminal_row in 0..height-1 {
             Terminal::clear_current_line();
-            if row == height / 3 {            
+            if let Some(row) = self.document.row(terminal_row as usize) {            
+                self.draw_row(row);            
+            } 
+            else if self.document.is_empty() && terminal_row == height / 3 {            
                 self.draw_welcome_message();         
             } else {            
                 println!("~\r");            
@@ -68,7 +91,7 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {  
         Terminal::cursor_hide();          
-        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default());
         if self.should_quit {    
             Terminal::clear_screen();        
             println!("Goodbye.\r");            
@@ -92,12 +115,24 @@ impl Editor {
     }
 
     fn move_cursor(&mut self, key: Key) {            
-        let Position { mut y, mut x } = self.cursor_position;            
+        let Position { mut y, mut x } = self.cursor_position; 
+        let size = self.terminal.size();            
+        let height = size.height.saturating_sub(1) as usize;            
+        let width = size.width.saturating_sub(1) as usize; 
+
         match key {            
             Key::Up => y = y.saturating_sub(1),            
-            Key::Down => y = y.saturating_add(1),            
+            Key::Down => {            
+                if y < height {            
+                    y = y.saturating_add(1);            
+                }            
+            },            
             Key::Left => x = x.saturating_sub(1),            
-            Key::Right => x = x.saturating_add(1),            
+            Key::Right => {            
+                if x < width {            
+                    x = x.saturating_add(1);            
+                }            
+            }            
             _ => (),            
         }            
         self.cursor_position = Position { x, y }            
--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,7 +1,11 @@
 mod editor;
 mod terminal;
+mod document;
+mod row;
 
+pub use document::Document;
 pub use terminal::Terminal;
+pub use row::Row;
 use editor::Editor;
 pub use editor::Position;
 
